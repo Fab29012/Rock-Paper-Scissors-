4813773462f888f6d9d04a3505f35662768e9646
Fabienne <101801681+Fab29012@users.noreply.github.com>
2025-01-28 01:07:19 +0100

    Started with computer choice and then human choice

--- game.js
+++ game.js
@@ -1,1 +1,29 @@
 console.log("Hello World")
+
+function getHumanChoice() {
+    const validChoices = ["Rock", "Paper", "Scissors"];
+    let userChoice = prompt("Rock, Paper or Scissors: ");
+
+    while (!validChoices.includes(userChoice)) {
+        userChoice = prompt("Invalid choice. Please choose Rock, Paper or Scissors: ");
+    }
+    return userChoice;
+}
+
+const userChoice = getHumanChoice();
+console.log ("You chose:", userChoice);
+
+function getComputerChoice() {
+    const randomGame = Math.floor (Math.random() * 3);
+    switch (randomGame) {
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
+    }
+}
+
+console.log(getComputerChoice());
+
