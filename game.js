3973c15c61d4cb8f225053e98d55d2bf8be0b2d7
Fabienne <101801681+Fab29012@users.noreply.github.com>
2025-01-29 01:28:22 +0100

    Added human choice and compute choice, set variables to 0 with let so it can be changed later.

--- game.js
+++ game.js
@@ -2,16 +2,16 @@ console.log("Hello World")
 
 function getHumanChoice() {
     const validChoices = ["Rock", "Paper", "Scissors"];
-    let userChoice = prompt("Rock, Paper or Scissors: ");
+    let humanChoice = prompt("Rock, Paper or Scissors: ");
 
-    while (!validChoices.includes(userChoice)) {
-        userChoice = prompt("Invalid choice. Please choose Rock, Paper or Scissors: ");
+    while (!validChoices.includes(humanChoice)) {
+        humanChoice = prompt("Invalid choice. Please choose Rock, Paper or Scissors: ");
     }
-    return userChoice;
+    return humanChoice;
 }
 
-const userChoice = getHumanChoice();
-console.log ("You chose:", userChoice);
+const humanChoice = getHumanChoice();
+console.log("You chose:", humanChoice);
 
 function getComputerChoice() {
     const randomGame = Math.floor (Math.random() * 3);
@@ -24,6 +24,28 @@ function getComputerChoice() {
             return "Scissors";
     }
 }
+const computerChoice = getComputerChoice();
+console.log("Computer chose:", computerChoice);
 
-console.log(getComputerChoice());
+    let humanScore = 0;
+    let computerScore = 0;
 
+    function playRound(humanChoice, computerChoice) {
+
+        humanChoice = humanChoice.toLowerCase();
+        computerChoice = computerChoice.toLowerCase();
+
+        if (humanChoice === computerChoice) {
+            return "It's a tie";
+        } else if (
+            (humanChoice === "rock" && computerChoice === "scissors") ||
+            (humanChoice === "scissors" && computerChoice === "paper") ||
+            (humanChoice === "paper" && computerChoice === "rock")
+        ) {
+            return "Human wins!";
+        } else {
+            return "Computer wins!";
+        }
+    }
+
+    console.log(playRound(humanChoice, computerChoice));
